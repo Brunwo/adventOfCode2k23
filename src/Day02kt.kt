a6fcb3046630867708501d211760c1390588507f
Brunwo <bwagner57@gmail.com>
2023-12-02 22:20:54 +0100

    Day 2, part 2

--- src/Day02kt.kt
+++ src/Day02kt.kt
@@ -1,21 +1,15 @@
+import kotlin.math.max
 
 fun main() {
 
-    val groundTruth = Color(12,13,14)
-//    val maxRed = 12
-//    val maxGreen = 13
-//    val maxBlue = 14
-
     fun part1(games: List<String>): Int {
 
-        // sample game line to parse :
-        // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        val maxPick = CubesPick(12, 13, 14)
 
-        return games.sumOf  game@{   gameLine ->
-            "game iteration".println()
-        val (game, rounds) = gameLine.split(":")
+        return games.sumOf game@{ gameLine ->
+            val (game, rounds) = gameLine.split(":")
             rounds.split(";").forEach { round ->
-                val color = Color()
+                val picked = CubesPick()
                     .apply {
                         round.trim().split(",").forEach { c ->
                             val (value, name) = c.trim().split(" ")
@@ -26,17 +20,36 @@ fun main() {
                             }
                         }
                     }
-                   if(color.anyHigher(groundTruth)) {
-                       return@game 0
-                   }
+                if (picked.anyHigher(maxPick)) {
+                    return@game 0
+                }
             }
-            val (_, gameNumber ) = game.split(" ")
+            val (_, gameNumber) = game.split(" ")
             return@game gameNumber.toInt()
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(games: List<String>): Int {
+
+        return games.sumOf game@{ gameLine ->
+
+            var atLeastRed = 1
+            var atLeastGreen = 1
+            var atLeastBlue = 1
+
+            val (_, rounds) = gameLine.split(":")
+            rounds.split(";").map { round ->
+                round.trim().split(",").forEach { c ->
+                    val (value, name) = c.trim().split(" ")
+                    when (name) {
+                        "red" -> atLeastRed = max(atLeastRed, value.toInt())
+                        "green" -> atLeastGreen = max(atLeastGreen, value.toInt())
+                        "blue" -> atLeastBlue = max(atLeastBlue, value.toInt())
+                    }
+                }
+            }
+            atLeastRed * atLeastGreen * atLeastBlue
+        }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -44,21 +57,21 @@ fun main() {
     part1(testInput).println()
     check(part1(testInput) == 8)
 
-//    val test2 = readInput("Day02_test2")
-//    check(part2(test2) == 0)
+    val test2 = readInput("Day02_test")
+    check(part2(test2) == 2286)
 
     "part1:".println()
     part1(readInput("Day02")).println()
 
     "part2:".println()
     part2(readInput("Day02")).println()
-    check(part2(readInput("Day02")) == 0)
 
 }
 
-data class Color(var red  : Int = 0, var green: Int = 0, var blue: Int = 0) {
+data class CubesPick(var red: Int = 0, var green: Int = 0, var blue: Int = 0) {
 
-    fun anyHigher  (other: Color) : Boolean {
+    fun anyHigher(other: CubesPick): Boolean {
         return red > other.red || green > other.green || blue > other.blue
     }
+
 }
